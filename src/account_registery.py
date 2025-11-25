e6340a2b1ea2a0eab3ef8ca4ceb7b4974bb0e235
kubaxd8641 <jakubsobczyk484@gmail.com>
2025-11-25 15:29:24 +0100

    Feature 15

--- src/account_registery.py
+++ src/account_registery.py
@@ -18,3 +18,22 @@ class AccountRegistery:
             if acc.pesel == id_number:
                 return acc
         return None
+    
+    def update_account_by_id(self, id_number, name=None, surname=None):
+        account = self.find_by_id_number(id_number)
+        if account is None:
+            return False 
+        if name is not None:
+            account.first_name = name
+        if surname is not None:
+            account.last_name = surname
+        return True
+    
+    def delete_account_by_id(self, id_number):
+        account = self.find_by_id_number(id_number)
+        if account is None:
+            return False
+        
+        self.accounts.remove(account)
+        return True
+
